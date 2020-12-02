3dca2b5fe9eb22a05a6d0e742f5758594aad0fb8
Chenzhe741 <55753399+Chenzhe741@users.noreply.github.com>
2020-12-03 02:40:54 +0800

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -11,6 +11,3 @@ samples = choice(['R','G','B'], size=100, p=[0.2, 0.5, 0.3])
 
 print(samples)
 
-StringBuffer tmp = null ;
-tmp.append("a").append("b").append("c");
-System.out.println(tmp.toString());
